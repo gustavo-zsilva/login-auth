fdbd33ea5faf598b79843d7f0ed7f3f31d883bfb
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-09-17 00:27:49 -0300

    Project Finished

--- script.js
+++ script.js
@@ -1,36 +1,93 @@
-const triggerButtons = document.querySelectorAll('.change-screen')
-const loginForm = document.querySelector('form')
+// Variáveis
+const navigateScreenBtns = document.querySelectorAll('.change-screen')
+
 const loginContainer = document.querySelector('.login')
 const registerContainer = document.querySelector('.register')
 
-triggerButtons.forEach(btn => {
-    btn.addEventListener('click', (event) => renderAnimations(event))
-})
+const forms = document.querySelectorAll('form')
+const submitButtons = document.querySelectorAll('button[type=submit]')
 
-function renderAnimations(event) {
+let credentials = JSON.parse(localStorage.getItem('register_credentials')) || [];
+
+// Métodos
+navigateScreenBtns.forEach(btn => btn.addEventListener('click', changeScreen))
+forms.forEach(form => form.addEventListener('submit', submitFields))
+
+// Faz as mudanças do formulário de login para o de registro, e vice-versa.
+function changeScreen(event) {
     event.preventDefault();
 
+    loginContainer.classList.toggle('hide')
+    registerContainer.classList.toggle('hide')
+}
 
-    // if (loginContainer.classList.contains('up')) {
-    //     registerContainer.classList.add('up');
-    //     loginContainer.classList.remove('down')
+// Faz os testes iniciais para checar se os inputs poderão ser validados.
+function submitFields(event) {
+    event.preventDefault()
 
-    //     loginForm.classList.remove('hide')
-    //     registerContainer.classList.add('hide')
-    // }
+    const currentForm = event.target;
+    const formInputs = currentForm.querySelectorAll('section .field input')
 
-    // loginContainer.classList.add('up')
+    let isValidated = [];
 
+    formInputs.forEach(input => isValidated.push(input.value === ''))
+
+    if (isValidated.includes(true)) {
+        swal(
+            'Calma aí!',
+            'Preencha todos campos para continuar.',
+            'warning'
+        )
    
+    } else {
+        let formValues = [];
+       
+        formInputs.forEach(input => formValues.push(input.value));
 
-    // loginContainer.addEventListener('animationend', ({animationName}) => {
-    //     if (animationName === 'up') {
-    //         loginForm.classList.add('hide')
+        currentForm.classList.contains('register-form')
+        ? registerUser(formValues)
+        : loginUser(formValues)
+    }
+}
 
-    //         registerContainer.classList.add('down')
-    //         registerContainer.classList.remove('hide')
-    //         registerContainer.classList.add('up')
-    //     }
-    // })
+// Cuida de permitir ou não o registro de um usuário.
+function registerUser(formValues) {
+
+    if (formValues[2] !== formValues[3]) {
+        swal(
+            'Sua senha pode ter sido escrita errada.',
+            'Verifique-a novamente para continuar.',
+            'warning'
+        )
+
+        return;
+    }
+
+    localStorage.setItem('register_credentials', JSON.stringify(formValues))
+    location.reload();
+}
+
+// Cuida de permitir ou não o login de um usuário.
+async function loginUser(formValues) {
+
+    // Checa se o usuário e a senha vão ao encontro do registro.
+    if (formValues[0] === credentials[0] && formValues[1] === credentials[2]) {
+
+        await swal(
+            'Bom trabalho!',
+            'Você está logado agora!',
+            'success'
+        )
+
+        location.reload()
+    } else {
+
+        swal(
+            'Oops...',
+            `Parece que algo deu errado na autenticação. <br> Verifique suas credenciais mais uma vez.`,
+            'error'
+        )
+    }
+
 }
 
